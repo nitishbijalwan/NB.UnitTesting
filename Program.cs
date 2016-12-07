3e090307f6d8313c40e5f45be5d8354c0251e3dd
nitishbijalwan <v-nibija@microsoft.com>
2016-12-07 23:27:17 +0530

    Adding code in base.

--- Program.cs
+++ Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace NB.UnitTesting
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("enter two number separted by space:");
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(new char[] { ' ' },
+                                        StringSplitOptions.RemoveEmptyEntries);
+            double sum = Add(numbers);
+            Console.WriteLine("Sum= {0}", sum);
+        }
+
+        public static double Add(string[] numbers)
+        {
+            double sum = 0;
+            double result;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                double.TryParse(numbers[i], out result);
+                sum += result;
+            }
+            return sum;
         }
     }
 }
